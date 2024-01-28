0bc3e62ff8befc209998e173e5cd55e16df8ee49
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-28 16:45:10 +0800

    Pseudocode

--- air_canvas.py
+++ air_canvas.py
@@ -1,2 +1,17 @@
 # Computer Vision Project | Air Canvas | Project with code and best for resume | Python OpenCV Project
 # https://youtu.be/eHgvMAwfODw?si=bPUW9INCQsJAn799
+
+# Import Libraries
+# Make function for trackbar 
+# Create trackbars for marker color adjustments
+# Create arrays to handle color points of different colors
+# Indexes to mark the points in particular arrays of specific colour
+# Kernel to be used for dilation purpose 
+# Create the Canvas setup
+# Load the default webcam of the laptop/PC
+# Add the color buttons to the live frame for color access
+# Identify the pointer by making its mask
+# Find contours for the pointer 
+# Draw lines of all the colors on the canvas and frame 
+# Display the windows
+# Display the camera and all resources
